b18f781f955b4a110e8da371d82a60618a200f4c
adhyayana108 <adhyayana108@gmail.com>
2026-08-15 10:13:05 +0530

    repository.go

---
+++ internal/repository/repository.go
@@ -0,0 +1,20 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/adhyayana108/movieq/internal/domain"
+)
+
+type MovieRepository interface {
+	GetAll(ctx context.Context) ([]domain.Movie, error)
+
+	GetByID(ctx context.Context, id string) (domain.Movie, error)
+
+	Create(ctx context.Context, movie domain.Movie) (domain.Movie, error)
+
+	Update(ctx context.Context, id string, movie domain.Movie) (domain.Movie, error)
+
+	Delete(ctx context.Context, id string) error
+	
+}
